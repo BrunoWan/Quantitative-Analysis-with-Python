41b8a257c7114fbd0af9b0ea50b31e08b7c560f3
Yun Wan <yunw@cs.dal.ca>
2018-01-30 14:08:21 -0500

    Update analysis.py

--- assess_portfolio/analysis.py
+++ assess_portfolio/analysis.py
@@ -5,8 +5,7 @@ import numpy as np
 import datetime as dt
 from util import get_data, plot_data
 
-# This is the function that will be tested by the autograder
-# The student must update this code to properly implement the functionality
+
 def assess_portfolio(sd = dt.datetime(2008,1,1), ed = dt.datetime(2009,1,1), \
     syms = ['GOOG','AAPL','GLD','XOM'], \
     allocs=[0.1,0.2,0.3,0.4], \
@@ -54,12 +53,7 @@ def assess_portfolio(sd = dt.datetime(2008,1,1), ed = dt.datetime(2009,1,1), \
     return cr, adr, sddr, sr, ev
 
 def test_code():
-    # This code WILL NOT be tested by the auto grader
-    # It is only here to help you set up and test your code
 
-    # Define input parameters
-    # Note that ALL of these values will be set to different values by
-    # the autograder!
     start_date = dt.datetime(2009,1,1)
     end_date = dt.datetime(2010,1,1)
     symbols = ['GOOG', 'AAPL', 'GLD', 'XOM']
